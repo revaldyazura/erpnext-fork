c343557d063dc3d24cefc1c71228de6960550c13
revaldyazura <raihanazura7@gmail.com>
2025-11-10 14:03:33 +0700

    refactor designation form setup to prefill Skill.designation from skills child table

--- erpnext/setup/doctype/designation/designation.js
+++ erpnext/setup/doctype/designation/designation.js
@@ -2,5 +2,17 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Designation", {
-	refresh: function (frm) {},
+	setup(frm) {
+		// When creating a new Skill from the 'skills' child table, prefill the Skill.designation
+		if (frm.fields_dict.skills && frm.fields_dict.skills.grid) {
+			const grid = frm.fields_dict.skills.grid;
+			const link_field = grid.get_field("skill");
+			if (link_field) {
+				link_field.get_route_options_for_new_doc = function () {
+					return { designation: frm.doc.name };
+				};
+			}
+		}
+	},
+	refresh(frm) {},
 });
